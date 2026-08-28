e5ab1967971726802b87d4ec8c5bc8ec39eb83c6
cmejia99 <cmejia99@gmail.com>
2026-08-28 17:16:39 -0500

    refactor(counter): optimize sprite URL handling and improve layout styles

--- components/counter.tsx
+++ components/counter.tsx
@@ -3,25 +3,19 @@
 import { useState } from "react";
 
 const MAX_POKEMON = 1025;
+const SPRITES_URL =
+  "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon";
 
 export default function Counter() {
   const [count, setCount] = useState(1);
 
   const pokemonId = ((count - 1) % MAX_POKEMON) + 1;
-  const spriteUrl = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${pokemonId}.png`;
 
   return (
-    <div
-      className="fade-in"
-      style={{
-        display: "flex",
-        justifyContent: "center",
-        padding: "48px 16px",
-      }}
-    >
+    <div className="fade-in" style={{ padding: "48px 16px" }}>
       <div
         className="card"
-        style={{ textAlign: "center", maxWidth: 360, width: "100%" }}
+        style={{ textAlign: "center", maxWidth: 360, margin: "0 auto" }}
       >
         <div className="kicker pixel neon-cyan" style={{ marginBottom: 16 }}>
           ▸ CONTADOR
@@ -36,17 +30,17 @@ export default function Counter() {
 
         <img
           key={pokemonId}
-          src={spriteUrl}
+          src={`${SPRITES_URL}/${pokemonId}.png`}
           alt={`Pokémon #${pokemonId}`}
           width={160}
           height={160}
-          style={{ imageRendering: "pixelated", margin: "0 auto 24px" }}
+          style={{ imageRendering: "pixelated", marginBottom: 24 }}
         />
 
         <button
           className="btn xl press"
           type="button"
-          onClick={() => setCount((c) => c + 1)}
+          onClick={() => setCount((actual) => actual + 1)}
         >
           ▶ INCREMENTAR
         </button>
